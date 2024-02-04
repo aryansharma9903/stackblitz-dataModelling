d4ff756c697a0b354df251a53f3532686d85fb60
aryansharma9903 <asharma32_be21@thapar.edu>
2024-02-04 15:31:33 +0530

    hello

--- models/hospital management/doctor.models.js
+++ models/hospital management/doctor.models.js
@@ -1,5 +1,29 @@
 import mongoose from 'mongoose';
 
-const doctorSchema = new mongoose.Schema({}, { timestamps: true });
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    salary:{
+        type: Number,
+        required:true,
+    },
+    qualifications: {
+        type: String,
+        required:true,
+    },
+    experience: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    worksinHospitals: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital",
+        }
+    ]
+}, { timestamps: true });
 
 export const Doctor = mongoose.model('Doctor', doctorSchema);
